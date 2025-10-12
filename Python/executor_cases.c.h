62c81827c441e5f5b49f69de82e5245af3f08fb3
Chris Eibl <138194463+chris-eibl@users.noreply.github.com>
2025-10-12 20:50:30 +0200

    reduce diff to main. Release and PGO still build.

--- Python/executor_cases.c.h
+++ Python/executor_cases.c.h
@@ -2132,7 +2132,7 @@
 
         case _LOAD_BUILD_CLASS: {
             _PyStackRef bc;
-            PyObject *Py_MSVC_RESTRICT bc_o;
+            PyObject *bc_o;
             _PyFrame_SetStackPointer(frame, stack_pointer);
             int err = _PyEval_Mapping_GetOptionalItem(BUILTINS(), &_Py_ID(__build_class__), &bc_o);
             stack_pointer = _PyFrame_GetStackPointer(frame);
@@ -2640,7 +2640,7 @@
             _PyStackRef value;
             oparg = CURRENT_OPARG();
             class_dict_st = stack_pointer[-1];
-            PyObject *Py_MSVC_RESTRICT value_o;
+            PyObject *value_o;
             PyObject *name;
             PyObject *class_dict = PyStackRef_AsPyObjectBorrow(class_dict_st);
             assert(class_dict);
@@ -3042,7 +3042,7 @@
         }
 
         case _SETUP_ANNOTATIONS: {
-            PyObject *Py_MSVC_RESTRICT ann_dict;
+            PyObject *ann_dict;
             if (LOCALS() == NULL) {
                 _PyFrame_SetStackPointer(frame, stack_pointer);
                 _PyErr_Format(tstate, PyExc_SystemError,
@@ -3255,7 +3255,7 @@
             int method_found = 0;
             PyObject *attr_o;
             Py_BEGIN_LOCALS_MUST_NOT_ESCAPE();
-            int *Py_MSVC_RESTRICT method_found_ptr = &method_found;
+            int *method_found_ptr = &method_found;
             _PyFrame_SetStackPointer(frame, stack_pointer);
             attr_o = _PySuper_Lookup(cls, self, name,
                                      Py_TYPE(self)->tp_getattro == PyObject_GenericGetAttr ? method_found_ptr : NULL);
@@ -4684,7 +4684,7 @@
             (void)lasti;
             PyObject* res_o;
             Py_BEGIN_LOCALS_MUST_NOT_ESCAPE();
-            PyObject *Py_MSVC_RESTRICT stack[5] = {NULL, PyStackRef_AsPyObjectBorrow(exit_self), exc, val_o, tb};
+            PyObject *stack[5] = {NULL, PyStackRef_AsPyObjectBorrow(exit_self), exc, val_o, tb};
             int has_self = !PyStackRef_IsNull(exit_self);
             _PyFrame_SetStackPointer(frame, stack_pointer);
             res_o = PyObject_Vectorcall(exit_func_o, stack + 2 - has_self,
